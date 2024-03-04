9a5d47c2edb1069a003cd7d8ab809dd0059602e2
Caio Beraldi Ribeiro <44821827+caiobribeiro@users.noreply.github.com>
2024-03-04 14:14:36 -0300

    Lcov coverage reader refactored

--- lib/lcov_reader.dart
+++ lib/lcov_reader.dart
@@ -10,6 +10,25 @@ void main(List<String> args) {
 String coverage({required String lcovPath}) {
   final file = File(lcovPath);
   final content = file.readAsLinesSync();
+  final reports = contentToLineReports(content);
+  final percent = calculatePercent(reports);
+
+  return '${percent.toString()}%';
+}
+
+int calculatePercent(List<LineReport> reports) {
+  int totalLineFound = 0;
+  int totalLineHit = 0;
+
+  for (var report in reports) {
+    totalLineFound += report.lineFound;
+    totalLineHit += report.lineHit;
+  }
+  final percent = (totalLineHit / totalLineFound) * 100;
+  return percent.round();
+}
+
+List<LineReport> contentToLineReports(List<String> content) {
   final reports = <LineReport>[];
   var sourceFile = '';
   var lineFound = 0;
@@ -34,14 +53,5 @@ String coverage({required String lcovPath}) {
       lineHit = int.parse(line[1]);
     }
   }
-
-  int totalLineFound = 0;
-  int totalLineHit = 0;
-
-  for (var report in reports) {
-    totalLineFound += report.lineFound;
-    totalLineHit += report.lineHit;
-  }
-  final percent = (totalLineHit / totalLineFound) * 100;
-  return '${percent.round().toString()}%';
+  return reports;
 }
--- test/lcov_reader_test.dart
+++ test/lcov_reader_test.dart
@@ -1,9 +1,18 @@
 import 'package:flutterando_tests/lcov_reader.dart';
+import 'package:flutterando_tests/models/line_report.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Should get coverage porcentage', () async {
     final result = coverage(lcovPath: './coverage/lcov.info');
     expect(result, '100%');
+  });
+
+  test('Should calculate 50% percentage', () async {
+    final result = calculatePercent([
+      LineReport(sourceFile: 'a', lineFound: 10, lineHit: 5),
+      LineReport(sourceFile: 'b', lineFound: 18, lineHit: 9),
+    ]);
+    expect(result, 50);
   });
 }
